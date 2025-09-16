52b3d1eafd1d830bfb15595c8c67d02b889e5e02
Suraj Kumar <suraj.20227053@mnnit.ac.in>
2025-09-17 00:26:37 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,21 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
-import cartRoutes from "./routes/cart.route.js"
+import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
 
-
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json({limit : "10mb"}));
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -29,14 +29,15 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*",(req,res) =>{
-        res.sendFile(path.resolve(__dirname,"frontend","dist", "index.html"));
-    })
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-app.listen(PORT,() =>{
-    connectDB()
-    console.log("server is running on http://localhost:" + PORT);
+app.listen(PORT, () => {
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
 });
